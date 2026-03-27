ab600ed7e2a0fd80c139bc2946c1b04dfb5e3e82
simeonadjei <simeonadjei12015@gmail.com>
2026-03-27 04:29:33 +0000

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-const scriptUrl = 'https://script.google.com/macros/s/AKfycbywG3WFQYtsrixlYXOe-O_sMSllbe4kGMMu8BUTNtJmVczfXE7fps_5GT26iMVtH16A/exec';
-const proxyUrl = `https://corsproxy.io/?${encodeURIComponent(scriptUrl)}`;
-
-const res = await fetch(proxyUrl, {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify(payload)  // Your form data
-});
